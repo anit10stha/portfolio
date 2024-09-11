da6bc27c4654888a2f667c2e3ab3b024d37ca844
Anit Shrestha <anitshrestha10@gmail.com>
2024-09-11 23:59:32 +0545

    custom cursor

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+// cursor animatin
+var body = document.querySelector("body");
+var cursor = document.querySelector("#cursor");
+
+body.addEventListener("mousemove", function(dets){
+    cursor.style.left = dets.clientX + "px";
+    cursor.style.top = dets.clientY + "px";
+})
+
+// for animation of icons, heading and paragraph
 gsap.from(".right-social a i",{
     x:200,
     y:0,
@@ -12,8 +22,9 @@ gsap.from(".heading",{
     y:2000,
     opacity: 0,
     duration: 1.5,
-    delay: 1,
+    delay: 3,
     stagger: 1
+    
 })
 
 gsap.from(".paragraph",{
@@ -21,6 +32,6 @@ gsap.from(".paragraph",{
     y:200,
     opacity: 0,
     duration: 1.5,
-    delay: 1,
+    delay: 3,
     stagger: 1
 })
