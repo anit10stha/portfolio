818b604a5b4df9dd54d5f59a14f96ebce30c4ffa
Anit Shrestha <anitshrestha10@gmail.com>
2024-09-11 16:43:23 +0545

    gsap added

---
+++ script.js
@@ -0,0 +1,31 @@
+// gsap.from(".coder img",{
+//     x:2000,
+//     y:200,
+//     duration: 1,
+//     delay: 1
+// })
+
+// gsap.from(".container-txt p",{
+//     x:-2000,
+//     y:200,
+//     duration: 1.5,
+//     delay: 0.5
+// })
+
+gsap.from(".heading",{
+    x:200,
+    y:2000,
+    opacity: 0,
+    duration: 1.5,
+    delay: 1,
+    stagger: 1
+})
+
+gsap.from(".paragraph",{
+    x:-200,
+    y:200,
+    opacity: 0,
+    duration: 1.5,
+    delay: 1,
+    stagger: 1
+})
